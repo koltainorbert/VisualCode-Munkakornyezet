be9293e5bb41764e4ba1eb94cc8f06e4666806dc
koltainorbert <koltainorbert@users.noreply.github.com>
2026-04-15 09:45:23 +0200

    Auto-mentĂ©s 09:45 - .vscode/notify.ps1

--- .vscode/notify.ps1
+++ .vscode/notify.ps1
@@ -4,13 +4,20 @@ param(
 )
 
 try {
+    # Banner popup engedélyezése registry-ben (önjavító)
+    $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe"
+    if (-not (Test-Path $regPath)) { New-Item -Path $regPath -Force | Out-Null }
+    Set-ItemProperty -Path $regPath -Name "ShowBanner" -Value 1 -Type DWord -ErrorAction SilentlyContinue
+    Set-ItemProperty -Path $regPath -Name "Enabled"    -Value 1 -Type DWord -ErrorAction SilentlyContinue
+
     [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
     [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] | Out-Null
 
-    $xml = "<toast><visual><binding template='ToastGeneric'><text>$Title</text><text>$Message</text></binding></visual></toast>"
+    $xml = "<toast duration='long'><visual><binding template='ToastGeneric'><text>$Title</text><text>$Message</text></binding></visual></toast>"
     $doc = [Windows.Data.Xml.Dom.XmlDocument]::new()
     $doc.LoadXml($xml)
     $toast = [Windows.UI.Notifications.ToastNotification]::new($doc)
     [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe").Show($toast)
+    Start-Sleep -Milliseconds 500
 } catch {}
 
